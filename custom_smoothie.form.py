0eb59f66034e2d2107ec95eac9b363e3c65acb88
mattrlong <mr.matt.r.long@gmail.com>
2025-07-16 11:50:02 +0100

    Update custom_smoothie.form.py

--- custom_smoothie.form.py
+++ custom_smoothie.form.py
@@ -17,7 +17,6 @@ st.write("The name on the order will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
@@ -25,23 +24,22 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 if ingredients_list: 
-    # st.write(ingredients_list)
-    # st.text(ingredients_list)
 
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-    # st.write(ingredients_string)
-
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
-    # st.write(my_insert_stmt)
-    # st.stop()
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order+ '!', icon="✅")
+
+# New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_reponse = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
